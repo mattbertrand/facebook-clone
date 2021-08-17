71776a8422f9f0517a47e60682934151aff05e2a
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-08-17 14:09:57 -0700

    styled the sidebar

--- src/components/SidebarRow.js
+++ src/components/SidebarRow.js
@@ -4,7 +4,7 @@ import '../css/SidebarRow.css';
 
 function SidebarRow({src, Icon, title}) {
     return (
-        <div>
+        <div className="sidebarRow">
             {src && <Avatar src={src} />}
             {Icon && <Icon />}
             <p>{title}</p>
