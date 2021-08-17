f2e00e1656920811b8082535da2e8d0343b8bfbc
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-08-17 16:49:43 -0700

    passed props to children component

--- src/components/StoryReel.js
+++ src/components/StoryReel.js
@@ -1,10 +1,30 @@
 import React from 'react'
 import '../css/StoryReel.css';
+import Story from './Story';
 
 function StoryReel() {
     return (
         <div className="storyReel">
-            
+            <Story 
+            image="https://scontent-sjc3-1.xx.fbcdn.net/v/t1.6435-9/81313990_10157993830197360_4720805044176289792_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=hnlgIWmBiagAX_3cIta&_nc_ht=scontent-sjc3-1.xx&oh=1696538035e3368dce1ea277faaef5a3&oe=6142CDC4"
+            profileSrc="https://scontent-sjc3-1.xx.fbcdn.net/v/t1.6435-9/77229474_10220340184484750_1405554173902061568_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=174925&_nc_ohc=lqqts8fmZAoAX9N67uD&_nc_ht=scontent-sjc3-1.xx&oh=c7bac4f9b99fb27f6f4873d0c791f681&oe=61438BC4"
+            title="Michael Gounon"
+            />
+            <Story 
+            image="https://scontent-sjc3-1.xx.fbcdn.net/v/t1.18169-9/14956051_10153856724790458_5638294192775063718_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=TSvvKkZPx0QAX8Pj5rE&_nc_ht=scontent-sjc3-1.xx&oh=16af473b85baf757be3ab601e815e3c4&oe=614369EA"
+            profileSrc="https://scontent-sjc3-1.xx.fbcdn.net/v/t31.18172-8/19055299_10156393505384835_21642295677283761_o.jpg?_nc_cat=107&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=qY0cGDeQ6C4AX9Ne7bI&_nc_ht=scontent-sjc3-1.xx&oh=cabcad516ee15fff3376769db0dbf109&oe=61414E4B"
+            title="Kevin Brundu"
+            />
+            <Story 
+            image="https://scontent-sjc3-1.xx.fbcdn.net/v/t1.6435-9/206058750_10159323199709036_5264318463737269995_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=e3f864&_nc_ohc=p4i_RfTRdYwAX8th7Ln&_nc_ht=scontent-sjc3-1.xx&oh=12e881f5ef0152ab103178165c4e50b0&oe=61416731"
+            profileSrc="https://scontent-sjc3-1.xx.fbcdn.net/v/t1.6435-9/207644912_10159319400879036_7198664494265562156_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=QsBU37I1GiIAX9_wCvt&tn=pqbk6lAY3_AluJm5&_nc_ht=scontent-sjc3-1.xx&oh=8f5190018208d0fc0c3f17c8b9f0988b&oe=6141C164"
+            title="Frederic Mey"
+            />
+            <Story 
+            image="https://scontent-sjc3-1.xx.fbcdn.net/v/t39.30808-6/s1080x2048/225917568_10208337604835224_8912128851772549768_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=730e14&_nc_ohc=UjvfKwBwqtsAX9aLEub&_nc_ht=scontent-sjc3-1.xx&oh=cb83e4b82972a512527f09157203e396&oe=61216D20"
+            profileSrc="https://scontent-sjc3-1.xx.fbcdn.net/v/t1.18169-9/28166526_1589987047788525_2102616087523548806_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=NrjJjl_KiV0AX9T5XkS&tn=pqbk6lAY3_AluJm5&_nc_ht=scontent-sjc3-1.xx&oh=8c0158176adf55fa35899541438ac430&oe=61401D42"
+            title="Kimberly Kingsbury"
+            />
         </div>
     )
 }
