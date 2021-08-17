750e75920f6b64758ab833a2accc90886d4b99b0
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-08-17 12:26:48 -0700

    created header and imported facebook logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="app">
       <h1>Let's build a Facebook clone!</h1>
+
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../css/Header.css';
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="header__left"></div>
+            <img src="https://1000logos.net/wp-content/uploads/2016/11/fb-logo.jpg" alt="facebook-logo"/>
+
+            <div className="header__middle"></div>
+
+            <div className="header__right"></div>
+        </div>
+    )
+}
+
+export default Header
