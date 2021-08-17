44e1916acd71dc5c7eb053e7b9748c15d104a3a5
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-08-17 13:02:46 -0700

    imported the search icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import '../css/Header.css';
+import SearchIcon from "@material-ui/icons/Search";
 
 function Header() {
     return (
         <div className="header">
             <div className="header__left">
                 <img src="https://1000logos.net/wp-content/uploads/2016/11/fb-logo.jpg" alt="facebook-logo"/>
+                <div className="header__input">
+                    <SearchIcon />
+                    <input type="text"></input>
+                </div>
             </div>
 
-            <div className="header__middle"></div>
+            <div className="header__middle">
+                
+            </div>
 
             <div className="header__right"></div>
         </div>
