4dc8d6486cdf9f603127f9a6358ea5dffff4e556
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-08-17 12:14:04 -0700

    created a config file for firebase

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDEaZW02x9pqO1mfqi-n0pBppxWiffCfTo",
+    authDomain: "facebook-clone-f8893.firebaseapp.com",
+    projectId: "facebook-clone-f8893",
+    storageBucket: "facebook-clone-f8893.appspot.com",
+    messagingSenderId: "903690362358",
+    appId: "1:903690362358:web:146cf4fdc0fe526bf37bd7",
+    measurementId: "G-80DNQRH0WJ"
+  };
