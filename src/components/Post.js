dd31099c4477d14364e1a5d6df1b5475efb5f690
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-08-18 11:34:23 -0700

    imported bottom icons and styled

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,6 +1,11 @@
 import { Avatar } from '@material-ui/core';
 import React from 'react';
-import '../css/Post.css'
+import '../css/Post.css';
+import ThumbUpIcon from '@material-ui/icons/ThumbUp';
+import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import NearMeIcon from '@material-ui/icons/NearMe';
+import { ExpandMoreOutlined } from '@material-ui/icons';
 
 function Post({ profilePic, image, username, timestamp, message }) {
     return (
@@ -21,6 +26,25 @@ function Post({ profilePic, image, username, timestamp, message }) {
 
             <div className="post__image">
                 <img src={image} alt="" />
+            </div>
+
+            <div className="post__options">
+                <div className="post__option">
+                    <ThumbUpIcon />
+                    <p>Like</p>
+                </div>
+                <div className="post__option">
+                    <ChatBubbleOutlineIcon />
+                    <p>Comment</p>
+                </div>
+                <div className="post__option">
+                    <NearMeIcon />
+                    <p>Share</p>
+                </div>
+                <div className="post__option">
+                    <AccountCircleIcon />
+                    <ExpandMoreOutlined />
+                </div>
             </div>
         </div>
     )
