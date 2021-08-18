bf12898b62fe0489d4534ceff1771d782d30ded7
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-08-18 11:07:32 -0700

    created a post component in the feed

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -2,12 +2,17 @@ import React from 'react';
 import '../css/Feed.css';
 import StoryReel from './StoryReel';
 import MessageSender from './MessageSender';
+import Post from './Post';
 
 function Feed() {
     return (
         <div className="feed">
             <StoryReel />
             <MessageSender />
+
+            <Post />
+            <Post />
+            <Post />
         </div>
     )
 }
