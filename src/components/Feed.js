1754e4aa0aae2214ba508b180fce455e1f8ffa48
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-08-17 17:21:29 -0700

    created a messageSender component and imported Icons

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,11 +1,13 @@
-import React from 'react'
-import '../css/Feed.css'
-import StoryReel from './StoryReel'
+import React from 'react';
+import '../css/Feed.css';
+import StoryReel from './StoryReel';
+import MessageSender from './MessageSender';
 
 function Feed() {
     return (
         <div className="feed">
             <StoryReel />
+            <MessageSender />
         </div>
     )
 }
