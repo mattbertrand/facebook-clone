c7ee36445e6293f0c015ed4f5f0912adf484f06b
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-08-18 14:43:40 -0700

    imported user data to every component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -8,11 +8,14 @@ import ChatIcon from '@material-ui/icons/Chat';
 import StorefrontIcon from '@material-ui/icons/Storefront';
 import VideoLibraryIcon from '@material-ui/icons/VideoLibrary';
 import { ExpandMoreOutlined} from '@material-ui/icons';
+import { useStateValue } from './StateProvider';
 
 function Sidebar() {
+    const [ {user}, dispatch] = useStateValue();
+
     return (
         <div className="sidebar">
-            <SidebarRow src="https://scontent-sjc3-1.xx.fbcdn.net/v/t1.6435-9/38144352_10156663207947360_8248451771043676160_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=w9AjAf5YZjAAX9kquoM&_nc_ht=scontent-sjc3-1.xx&oh=f8f3d45b3a61edf9a7143da9409fe430&oe=6142054B" title="Matthieu Bertrand"/>
+            <SidebarRow src={user.photoURL} title={user.displayName}/>
             <SidebarRow Icon={LocalHospitalIcon} title="COVID-19 Information Center"/>
             <SidebarRow Icon={EmojiFlagsIcon} title="Pages"/>
             <SidebarRow Icon={PeopleIcon} title="Friends"/>
