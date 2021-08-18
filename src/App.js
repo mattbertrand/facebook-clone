bee6f3feabb17f4014d5e68c1584b23eee191ec1
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-08-18 12:59:13 -0700

    dispatch state to get the user data to login

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
+import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import Feed from './components/Feed';
 import Widgets from './components/Widgets';
 import Login from './components/Login';
+import { useStateValue } from './components/StateProvider';
 
 function App() {
 
-  const user = null;
+  const [ {user}, dispatch ] = useStateValue();
 
   return (
     <div className="app">
--- src/components/Login.js
+++ src/components/Login.js
@@ -2,12 +2,20 @@ import { Button } from '@material-ui/core';
 import React from 'react';
 import '../css/Login.css';
 import { auth, provider } from '../firebase';
+import { actionTypes } from './reducer';
+import { useStateValue } from './StateProvider';
 
 function Login() {
+
+    const [state, dispatch] = useStateValue();
 
     const signIn =() => {
         auth.signInWithPopup(provider)
         .then(result => {
+            dispatch({
+                type: actionTypes.SET_USER,
+                user: result.user,
+            })
             console.log(result.user)
         })
         .catch(error => alert(error.message))
